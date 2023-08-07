39cc55f7c2545b6db605863050b6284cbb65d469
(Jesus) <(ssdontletmed@gmail.com)>
2023-08-07 00:08:48 -0500

    this augst 7 2023

--- add-content.js
+++ add-content.js
@@ -57,28 +57,52 @@ function countClicks3()
     var hsrc = document.getElementById('hsrc');
     hsrc.src = 'scissors.webp';
     var getComputerChoice = Math.floor(Math.random()*9);
-    if (getComputerChoice >= 6 ){
+    if (hcount == 10 && getComputerChoice >= 6){
+        hcount -= 9;
+        ccount = 0;
+        human.innerHTML= hcount;
+        computer.innerHTML= ccount;
+        var cimg = document.getElementById('csrc');
+        cimg.src = 'paper.png';
+        dialog.innerHTML= 'your siscores really demolished my paper';
+    } else if (ccount == 10 && getComputerChoice <= 2){
+        ccount -= 9;
+        hcount = 0; 
+        computer.innerHTML= ccount;
+        human.innerHTML= hcount;
+        var cimg = document.getElementById('csrc');
+        cimg.src = 'rock.jpeg';
+        dialog.innerHTML= 'My big rock destroyed your sisscores';
+    } else if (getComputerChoice >= 6 && ccount!=10){
         hcount++ ;
         human.innerHTML= hcount;
         dialog.innerHTML= 'your siscores really demolished my paper';
         var cimg = document.getElementById('csrc');
         cimg.src = 'paper.png';
-    } else if (getComputerChoice <= 2){
+    } else if (getComputerChoice <= 2 && hcount!=10){
         ccount++ ; 
         computer.innerHTML= ccount;
         dialog.innerHTML= 'My big rock destroyed your sisscores';
         var cimg = document.getElementById('csrc');
         cimg.src = 'rock.jpeg';
-    } else {
+    } else if ((getComputerChoice < 6 && getComputerChoice > 2 && (hcount!= 10|| ccount!= 10))){
         dialog.innerHTML= 'I hate ties';
         var cimg = document.getElementById('csrc');
         cimg.src = 'scissors.webp'
+    } else {
+        human.innerHTML= hcount;
+        computer.innerHTML= ccount;
+        cimg.src = 'Psteve.jpeg';
+        hsrc.src = 'Psteve.jpeg';
+        dialog.innerHTML= 'seee see';
+
     }
+    console.log(hcount)
 }
 function finalSay()
 {
     if (ccount == 10){
-        dialog.innerHTML='I won yo mamam!!'
+        dialog.innerHTML='I won yo mamam!!';
     } else if (hcount == 10){
         dialog.innerHTML= "you win i guess"}
 }
